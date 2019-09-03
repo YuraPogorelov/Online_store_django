bc121126d21ad70ac9642dcf49a7b28da405d94e
YuraPogorelov <pogorelov.yurik@gmail.com>
2019-09-03 22:46:31 +0300

    установлена московское время

--- econapp/admin.py
+++ econapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Brand
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
--- econapp/models.py
+++ econapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
