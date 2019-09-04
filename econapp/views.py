fc54495260fb7181bd6ad0144fcf273f8ed9b734
Yura <pyurik44@gmail.com>
2019-09-04 21:47:59 +0300

    Вывод данных в шаблон

--- econapp/views.py
+++ econapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
 
 def base_view(request):
-    return render(request, 'base.html', {})
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context = {
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'base.html', context)
