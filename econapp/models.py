b1fb3cd92fec4d41250ad83066c58d9fcff394fc
Yura <pyurik44@gmail.com>
2019-09-04 21:57:50 +0300

    Переопределение менеджера модели Product

--- econapp/models.py
+++ econapp/models.py
@@ -29,6 +29,11 @@ def image_folder(instance, filename):
     filename = instance.slug + '.' + filename.split('.')[1]
     return "{0}/{1}".format(instance.slug, filename)
 
+class ProductManager(models.Manager):
+
+    def all(self, *args, **kwargs):
+        return super(ProductManager, self).get_queryset().filter(available=True)
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
@@ -38,6 +43,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to=image_folder)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     available = models.BooleanField(default=True)
+    objects = ProductManager()
     
     def __str__(self):
         return self.title
